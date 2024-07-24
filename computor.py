881a9a874f4e607c5c58d47c6348dd5be21a1174
Mathis CATAL <catalmathis@gmail.com>
2024-07-24 18:48:34 +0200

    commencement bonus

--- computor.py
+++ computor.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import re
 
 def parsing():
     parser = argparse.ArgumentParser(description='ComputorV1')
@@ -29,17 +30,25 @@ def simplify(equation):
     right = equation.split('=')[1]
     degree = more_than_2(left, right)
     sign = ""
+
+    print("left part:", left, "\nright part:", right, "\n")
     
-    sign = left.split('X^0')[0][0] if left.split('X^0')[0][0] == "-" else ""
-    c_left = float(left.split('X^0')[0].translate(translation_table))
-    c_left = c_left * -1 if sign == "-" else c_left
-    left, next = update_left_right(left, 'X^0', 'X^1')
+    try:
+        sign = left.split('X^0')[0][0] if left.split('X^0')[0][0] == "-" else ""
+        c_left = float(left.split('X^0')[0].translate(translation_table))
+        c_left = c_left * -1 if sign == "-" else c_left
+        left, next = update_left_right(left, 'X^0', 'X^1')
+        print("c_left:", to_int_if_possible(c_left), "\nleft:", left, "\n")
+    except:
+        c_left = 0
+        sign = ""
 
     try:
         sign = (left.split('X^1')[0][0])
         b_left = float(left.split('X^1')[0].translate(translation_table) if next == True else '0')
         b_left = b_left * -1 if sign == "-" else b_left
         left, next = update_left_right(left, 'X^1', 'X^2')
+        print("b_left:", to_int_if_possible(b_left), "\nleft:", left, "\n")
     except:
         b_left = 0
         sign = ""
@@ -49,21 +58,28 @@ def simplify(equation):
         a_left = float(left.split('X^2')[0].translate(translation_table) if next == True else '0')
         a_left = a_left * -1 if sign == "-" else a_left
         left, next = update_left_right(left, 'X^2', "")
+        print("a_left:", to_int_if_possible(a_left), "\nleft:", left, "\n")
     except:
         a_left = 0
         sign = ""
 
-    sign = (right.split('X^0')[0].strip()[0]) if right.split('X^0')[0].strip()[0] == "-" else ""
-    c_right = float(right.split('X^0')[0].translate(translation_table))
-    c_right = c_right * -1 if sign == "-" else c_right
-    right, next = update_left_right(right, 'X^0', 'X^1')
+    try:
+        sign = (right.split('X^0')[0].strip()[0]) if right.split('X^0')[0].strip()[0] == "-" else ""
+        c_right = float(right.split('X^0')[0].translate(translation_table))
+        c_right = c_right * -1 if sign == "-" else c_right
+        right, next = update_left_right(right, 'X^0', 'X^1')
+        print("c_right:", to_int_if_possible(c_right), "\nright:", right, "\n")
+    except:
+        c_right = 0
+        sign = ""
 
     try:
         sign = right.split('X^1')[0][0]
         b_right = float(right.split('X^1')[0].translate(translation_table) if next == True else '0')
         b_right = b_right * -1 if sign == "-" else b_right
         right, next = update_left_right(right, 'X^1', 'X^2')
-    except Exception as e:
+        print("b_right:", to_int_if_possible(b_right), "\nright:", right, "\n")
+    except:
         b_right = 0
         sign = ""
 
@@ -72,19 +88,24 @@ def simplify(equation):
         a_right = float(right.split('X^2')[0].translate(translation_table) if next == True else '0')
         a_right = a_right * -1 if sign == "-" else a_right
         right, next = update_left_right(right, 'X^2', "")
+        print("a_right:", to_int_if_possible(a_right), "\nright:", right, "\n")
     except:
         a_right = 0
-
-    # print(a_left, b_left, c_left, a_right, b_right, c_right)
 
     a = to_int_if_possible(a_left - a_right)
     b = to_int_if_possible(b_left - b_right)
     c = to_int_if_possible(c_left - c_right)
 
+    print("a:", a_left, "-", a_right, "\n")
+    print("b:", b_left, "-", b_right, "\n")
+    print("c:", c_left, "-", c_right, "\n")
+
     translation_table = str.maketrans('+-', '-+')
     right = right.translate(translation_table)
-    right = "" if right.replace(" ", "") == "0" else right
-    print("Reduced form:", c, "* X^0" + formate(b, degree, 1) + formate(a, degree, 2) + left + right + " = 0")
+    right = "" if right.replace(" ", "") == "0" or right == " " else right
+    right = " " + right + " " if right != "" else right
+    left = " " + left + " " if left != "" else left
+    print("Reduced form:", c, "* X^0" + formate(b, degree, 1) + formate(a, degree, 2) + left + right + "= 0")
 
     return a, b, c, degree
 
@@ -96,6 +117,8 @@ def formate(value, max_degree, degree):
         string = " + " + str(value) + " * X^" + str(degree)
     if value == 0 and degree >= max_degree:
         string = ""
+    if max_degree == 0 and degree == 1:
+        return " "
     if degree == max_degree:
         string = string + " "
     return string
@@ -115,8 +138,10 @@ def more_than_2(left, right):
     
 
 def compute_delta(a, b, c):
-    # print(a,b,c)
+    print("Δ = b² - 4ac")
+    print("Δ =", str(b) + "² - 4 *", a, "*", c)
     delta = (b ** 2) - (4 * a * c)
+    print("Δ =", delta)
     if delta > 0:
         print("Discriminant is strictly positive, the two solutions are:")
         print("X1 =", (-b + delta ** 0.5) / (2 * a))
@@ -139,6 +164,14 @@ def main():
             print('Usage: python3 computor.py "equation"')
             sys.exit(1)
         args = parsing()
+        
+        pattern = r"^\s*([+-]?\s*\d+(\.\d+)?\s*\*\s*X\^\d+\s*)+(\s*[+-]\s*\d+(\.\d+)?\s*\*\s*X\^\d+\s*)*\s*=\s*([+-]?\s*\d+(\.\d+)?\s*\*\s*X\^\d+\s*)+(\s*[+-]\s*\d+(\.\d+)?\s*\*\s*X\^\d+\s*)*\s*$"
+        good_eqution = bool(re.match(pattern, args.equation))
+        if not good_eqution:
+            raise Exception("Invalid equation")
+        
+        print("Equation to solve:", args.equation, "\n")
+        
         a, b, c, degree = simplify(args.equation)
         if degree > 2 :
             print("Polynomial degree:", degree)
